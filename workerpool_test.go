569a72e1c9d2c7865b0a125fccecd2148032e72d
Tobias Klauser <tobias@isovalent.com>
2021-02-05 15:56:09 +0100

    Test that New(n) with n ≤ 0 will panic
    Make sure that constructing a worker pool with zero or negative n will
    panic.
    
    Signed-off-by: Tobias Klauser <tobias@isovalent.com>

--- workerpool_test.go
+++ workerpool_test.go
@@ -23,6 +23,21 @@ import (
 	"time"
 )
 
+func testWorkerPoolNewPanics(t *testing.T, n int) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("New(%d) should panic()", n)
+		}
+	}()
+	_ = New(n)
+
+}
+
+func TestWorkerPoolNewPanics(t *testing.T) {
+	testWorkerPoolNewPanics(t, 0)
+	testWorkerPoolNewPanics(t, -1)
+}
+
 func TestWorkerPoolTasksCapacity(t *testing.T) {
 	wp := New(runtime.NumCPU())
 	defer wp.Close()
